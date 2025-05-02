1ad4fdb7c548d03352ca941242aa6ba8d4f537a3
Mikhail Eliseev <mikhail.yeliseev.04@mail.ru>
2025-05-02 21:39:55 +0400

    Основная структура программы

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,47 @@
 public class Main {
+    // Абстрактные классы (абстрактный продукт)
+    public static abstract class Soup {
+        public abstract String getDescription();
+    }
+
+    // Главное блюдо (абстрактный продукт)
+    public static abstract class MainCourse {
+        public abstract String getDescription();
+    }
+
+    // Абстрактная фабрика
+    public interface MealFactory {
+        Soup createSoup();
+        MainCourse createMainCourse();
+    }
+
+    // Конкретные фабрики
+    // Мясная диета
+    public static class MeatDietFactory implements MealFactory {
+        @Override
+        public Soup createSoup() {
+            return null;
+        }
+
+        @Override
+        public MainCourse createMainCourse() {
+            return null;
+        }
+    }
+
+    // Вегетерианская диета
+    public static class VeggieDietFactory implements MealFactory {
+        @Override
+        public Soup createSoup() {
+            return null;
+        }
+
+        @Override
+        public MainCourse createMainCourse() {
+            return null;
+        }
+    }
+    
     public static void main(String[] args) {
         System.out.println("Мясная диета");
         System.out.println("\nВегетерианская диета");
